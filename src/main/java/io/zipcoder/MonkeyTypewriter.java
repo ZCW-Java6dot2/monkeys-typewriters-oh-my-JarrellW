c70bdab0864f21134faffad7c4d3ce6156f74023
Jay <73120876+JarrellW@users.noreply.github.com>
2020-12-03 21:39:57 -0500

    Completed the right way

--- src/main/java/io/zipcoder/MonkeyTypewriter.java
+++ src/main/java/io/zipcoder/MonkeyTypewriter.java
@@ -1,7 +1,7 @@
 package io.zipcoder;
 
 public class MonkeyTypewriter {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         String introduction = "It was the best of times,\n" +
                 "it was the blurst of times,\n" +
                 "it was the age of wisdom,\n" +
@@ -23,16 +23,28 @@ public class MonkeyTypewriter {
         UnsafeCopier unsafeCopier = new UnsafeCopier(introduction);
         SafeCopier safeCopier = new SafeCopier(introduction);
 
-        Thread[] unsafeMonkeys = new Thread[5];
-        for (int i=0; i<5; i++){
-            unsafeMonkeys[i] = new Thread(unsafeCopier);
-            unsafeMonkeys[i].start();
-        }
+//        Thread[] unsafeMonkeys = new Thread[5];
+//        for (int i=0; i<5; i++){
+//            unsafeMonkeys[i] = new Thread(unsafeCopier);
+//            unsafeMonkeys[i].start();
+//        }
 
         Thread[] safeMonkeys = new Thread[5];
-        for (int i=0; i<5; i++){
+        for (int i = 0; i < 5; i++) {
             safeMonkeys[i] = new Thread(safeCopier);
+        }
+        for (int i = 0; i < safeMonkeys.length; i++) {
             safeMonkeys[i].start();
+
+        }
+        try {
+            for (int i = 0; i < safeMonkeys.length; i++) {
+                safeMonkeys[i].join();
+
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Main Interrupted");
+
         }
 
 
@@ -40,11 +52,23 @@ public class MonkeyTypewriter {
         // after enough time has passed.
         try {
             Thread.sleep(1000);
-        } catch(InterruptedException e) {
+        } catch (InterruptedException e) {
             System.out.println("MAIN INTERRUPTED");
         }
 
         System.out.println("Unsafe copy:\n" + unsafeCopier.copied + "\n");
         System.out.println("Safe copy:\n" + safeCopier.copied + "\n");
+
+        if (pageMatcher(safeCopier.copied, introduction)) {
+            System.out.println("A Match!");
+        } else {
+            System.out.println("Unsuccesful Copy");
+        }
+    }
+// Print out the copied versions here.
+
+    public static Boolean pageMatcher(String copy, String original) {
+        return copy.equals(original);
+
     }
 }
--- src/main/java/io/zipcoder/SafeCopier.java
+++ src/main/java/io/zipcoder/SafeCopier.java
@@ -1,5 +1,6 @@
 package io.zipcoder;
 
+import java.util.Random;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -7,22 +8,37 @@ import java.util.concurrent.locks.ReentrantLock;
  * Make this extend the Copier like `UnsafeCopier`, except use locks to make sure that the actual intro gets printed
  * correctly every time.  Make the run method thread safe.
  */
-public class SafeCopier extends Copier{
-    private final Lock lock = new ReentrantLock();
+public class SafeCopier extends Copier {
 
     public SafeCopier(String toCopy) {
         super(toCopy);
     }
 
     public void run() {
-        lock.lock();
-        try {
-            while (stringIterator.hasNext()){
-                copied += stringIterator.next() + " ";
+        Random random = new Random();
+        String next = "";
+
+
+        while (stringIterator.hasNext()) {
+            try {
+                synchronized (stringIterator) {
+                    if(stringIterator.hasNext()){
+                        next = stringIterator.next() + " ";
+                        Thread.sleep(random.nextInt((250 - 50) + 1) + 50);
+                        copied += next;
+                        System.out.println(Thread.currentThread().getName());
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        } finally {
-            lock.unlock();
+//        lock.lock();
+
         }
+        copied = copied.trim();
     }
 }
+
+
+
 
